932d6fd8836a564519ed829e60c61573bfc1c8ee
Santiago Garcia Rincon <santiagogr924@gmail.com>
2025-01-26 10:58:55 -0500

    chore: Remove obsolete test and run scripts

--- run_app.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-
-# Add the root directory to PYTHONPATH
-sys.path.append(os.path.dirname(__file__))
-
-# Run the Streamlit application
-os.system("streamlit run frontend/app.py")
